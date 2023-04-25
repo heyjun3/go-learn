a1bcffb40fecd59f86ac37547789d012ed4292c0
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-04-26 01:03:14 +0900

    update

---
+++ game/sequencing/double_buffer.go
@@ -0,0 +1,26 @@
+package sequencing
+
+
+type Frame struct {
+	pixles []int
+}
+
+func NewFrame(size int) *Frame{
+	return &Frame{
+		pixles: make([]int, size),
+	}
+}
+
+func (f *Frame) Clear() {
+	for i, _ := range f.pixles {
+		f.pixles[i] = 0
+	}
+}
+
+func (f *Frame) Draw(x int) {
+	f.pixles[x] = 1
+}
+
+func (f Frame) GetPixels() []int{
+	return f.pixles
+}
