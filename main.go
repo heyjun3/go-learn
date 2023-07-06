8a8f2894621db5fb7e4ff8ae3388b6b6802c24aa
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-07-07 00:32:05 +0900

    fix

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	fmt.Println("test")
 }
+
+
+type GetList[T Stringer] func() (T, error)
+
+type Stringer interface{}
+
+type Product struct {
+	GetList[Str]
+}
+
+func NewProduct() Product{
+	return Product{
+		GetList: func() (Str, error) { return Str{}, nil},
+	}
+}
+
+type Str struct {}
